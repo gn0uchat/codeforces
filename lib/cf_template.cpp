a38b91aac3e275372cdaf2286bc058e63906b4d3
gn0uchat <gn0uchat@gmail.com>
2019-12-26 22:33:09 +0800

    add field op into cf_template.cpp

--- lib/cf_template.cpp
+++ lib/cf_template.cpp
@@ -20,6 +20,42 @@ int mod( int v, int base ){
 	return ( ( v % base ) + base ) % base;
 }
 
+int fd_add( int a, int b, int prime ){
+	int r = ( a - prime) + b;
+	return r < 0 ? r + prime: r;
+}
+
+int fd_sub( int a, int b, int prime ){
+	int r = a - b;
+	return r < 0 ? r + prime : r;
+}
+
+int fd_mul( int a, int b, int prime ){
+	long long int big_a = a;
+	long long int big_b = b;
+	
+	long long int r = big_a * big_b;
+
+	return ((int)( ( big_a * big_b ) % prime ));
+}
+
+int fd_div( int dividend, int divisor, int prime ){
+	int t_inv = 0, inv = 1;
+	for( int v = prime, r = divisor; r > 1; ){
+		int q = v / r;
+		int new_r = v % r;
+		int new_inv = sub( t_inv, mul( q, inv ) );
+
+		t_inv = inv;
+		inv = new_inv;
+
+		v = r;
+		r = new_r;
+	}
+
+	return mul( dividend, inv );
+}
+
 int main(){
 	return 0;
 }
