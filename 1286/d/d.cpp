6db691c2691a22b690523f1280f888aa03b2c861
gn0uchat <gn0uchat@gmail.com>
2020-02-02 11:30:59 +0000

    problem 1286 D AC

--- 1286/d/d.cpp
+++ 1286/d/d.cpp
@@ -12,54 +12,93 @@
 #include<tuple>
 using namespace std;
 
-template<class T, int m, int n>
+template<class T, int n>
 class mtx_t{
 private:
-	vector< vector<T> > entry;
-
 public:
+	T entry[ n ][ n ];
+
 	mtx_t(){
-		this->entry.resize( m, vector< T >( n ));
 	}
 
 	mtx_t( initializer_list< initializer_list< T >> init_l ){
+		int i = 0;
 		for( auto v : init_l ){
-			this->entry.push_back( vector<T>( v ));
+			int j = 0;
+			for( auto s : v ){
+				this->entry[ i ][ j ] = s;
+				j ++;
+			}
+			i ++;
 		}
 	}
 
-	mtx_t operator* ( const mtx_t< T, m, n>& mp ){
-		mtx_t< T, m, n > r;
-		for( int i = 0; i < m; i ++ ){
+	friend mtx_t<T, n> operator* ( const mtx_t<T,n> &a, const mtx_t< T, n>&b ){
+		mtx_t< T, n > r;
+		for( int i = 0; i < n; i ++ ){
 			for( int j = 0; j < n; j ++ ){
 				for( int k = 0; k < n; k ++ ){
-					r[ i ][ j ] = r[ i ][ j ] + r[ i ][ k ] * mp[ k ][ j ];
+					r.entry[ i ][ j ] = r.entry[ i ][ j ] + ( a.entry[ i ][ k ] * b.entry[ k ][ j ] );
 				}
 			}
 		}
+		return r;
 	}
-
-	vector<T>& operator[] ( size_t i ){
-		return this->entry[ i ];
+	
+	T sum(){
+		auto acc = T();
+		for( int i = 0; i < n; i ++ ){
+			for( int j = 0; j < n; j ++ ){
+				acc = acc + this->entry[ i ][ j ];
+			}
+		}
+		return acc;
 	}
 };
 
-<template int prime>
+template< int prime >
 class mod_int_fd{
 private:
 	int elem;
-	int mod( int v, int base ){
-		return ( ( v % base ) + base ) % base;
+	int mod( long long int v ) const {
+		v = v % prime;
+		return v < 0 ? v + prime : v;
+	}
+public:
+
+	int operator* () const {
+		return this->elem;
 	}
 
-	int mul_inv( int a ){
-		a = mod( a, prime );
+	mod_int_fd<prime> operator+ ( const mod_int_fd<prime> &b ) const{
+		int x = this->elem;
+		int y = *b;
+		int r = ( x - prime ) + y;
+		return r < 0 ? mod_int_fd( r + prime ) : mod_int_fd( r );
+	}
+
+	mod_int_fd<prime> operator- ( const mod_int_fd<prime> &b ) const {
+		int x = elem;
+		int y = *b;
+		int r = x - y;
+		return r < 0 ? mod_int_fd<prime>( r + prime ): mod_int_fd<prime>( r );
+	}
+
+	mod_int_fd<prime> operator*( const mod_int_fd<prime> &b ) const {
+		long long int big_a = this->elem;
+		long long int big_b = *b;
+
+		return mod_int_fd<prime>(( big_a * big_b ) % prime );
+	}
+
+	mod_int_fd<prime> mul_inv() const {
+		int a = this->elem;
 
 		int t_inv = 0, inv = 1;
 		for( int v = prime, r = a; r > 1; ){
 			int q = v / r;
 			int new_r = v % r;
-			int new_inv = sub( t_inv, mul( q, inv ) );
+			int new_inv = mod( ((long long int) t_inv) - ( ((long long int) q) * ((long long int) inv)) );
 
 			t_inv = inv;
 			inv = new_inv;
@@ -68,42 +107,28 @@ private:
 			r = new_r;
 		}
 
-		return inv;
+		return mod_int_fd<prime>( inv );
 	}
 
-public:
-	int operator* (){
-		return this->elem;
+	mod_int_fd<prime> operator/ ( const mod_int_fd<prime> &b ) const {
+		return ( * this ) * b.mul_inv();
 	}
 
-	mod_int_fd<prime> operator+ ( const mod_int_fd<prime> &b ){
-		a = mod( this->elem, prime );
-		b = mod( *b, prime );
+	friend mod_int_fd<prime > operator* ( int v, const mod_int_fd<prime> &b ){ return mod_int_fd<prime>( v ) * b; }
+	friend mod_int_fd<prime > operator/ ( int v, const mod_int_fd<prime> &b ){ return mod_int_fd<prime>( v ) / b; }
+	friend mod_int_fd<prime > operator+ ( int v, const mod_int_fd<prime> &b ){ return mod_int_fd<prime>( v ) + b; }
+	friend mod_int_fd<prime > operator- ( int v, const mod_int_fd<prime> &b ){ return mod_int_fd<prime>( v ) - b; }
+	mod_int_fd<prime> operator * ( int v ) const { return this->elem * mod_int_fd<prime>( v ); }
+	mod_int_fd<prime> operator / ( int v ) const { return this->elem / mod_int_fd<prime>( v ); }
+	mod_int_fd<prime> operator + ( int v ) const { return this->elem + mod_int_fd<prime>( v ); }
+	mod_int_fd<prime> operator - ( int v ) const { return this->elem - mod_int_fd<prime>( v ); }
 
-		int r = ( a - prime) + b;
-		return r < 0 ? r + prime: r;
+	mod_int_fd(){
+		this->elem = 0;
 	}
 
-	mod_int_fd<prime> operator- ( const mod_int_fd<prime> &b ){
-		int a = elem;
-		int b = *b;
-		int r = a - b;
-		return r < 0 ? mod_int_fd<prime>( r + prime ): mod_int_fd<prime>( r );
-	}
-
-	mod_int_fd<prime> operator*( const mod_int_fd<prime> &b )
-		long long int big_a = this->elem;;
-		long long int big_b = *b;
-
-		return mod_int_fd<prime>((int)( ( big_a * big_b ) % prime ));
-	}
-
-	mod_int_fd<prime> operator/ ( const mod_int_fd<prime> &b )
-		return mod_int_fd<prime>( mul( this->elem, *b ) );
-	}
-
-	mod_int_fd( int elem ){
-		this->elem = mod( elem, prime );
+	mod_int_fd( long long int elem ){
+		this->elem = mod( elem );
 	}
 };
 
@@ -111,8 +136,8 @@ template<class T>
 class seg_tree{
 private:
 	typedef tuple<int, int> segment;
+
 	int alloc_i;
-	int low_bound, upp_bound;
 	vector< tuple< int, int, T > > mem;
 	function<T(const T&, const T&)> op;
 	function<T(int)> init_v;
@@ -146,10 +171,10 @@ private:
 			li = this->alloc_i ++;	
 			ri = this->alloc_i ++;
 
-			T lv = node_init( left_seg ( nd_seg ), li, init_v );
-			T rv = node_init( right_seg( nd_seg ), ri, init_v );
+			T lv = node_init( left_seg ( nd_seg ), li );
+			T rv = node_init( right_seg( nd_seg ), ri );
 
-			v = this->op( nd_seg, lv, rv );
+			v = this->op( lv, rv );
 			return v;
 		}
 	}
@@ -170,7 +195,7 @@ private:
 			}else{
 				T lv = this->node_query( left_seg( nd_seg ) , { qy_lb, mid }, li );
 				T rv = this->node_query( right_seg( nd_seg ), { mid, qy_ub }, ri );
-				return this->op( nd_seg, lv, rv );
+				return this->op( lv, rv );
 			}
 		}
 	}
@@ -187,18 +212,18 @@ private:
 			if( p < mid ){
 				T lv = this->node_update( left_seg( nd_seg ), p, li, update_v );
 				T rv = this->node_query( right_seg( nd_seg ), right_seg( nd_seg ), ri );
-				v = this->op( nd_seg, lv, rv );
+				v = this->op( lv, rv );
 			}else{
 				T lv = this->node_query( left_seg( nd_seg ), left_seg( nd_seg ), li );
 				T rv = this->node_update( right_seg( nd_seg), p, ri, update_v );
-				v = this->op( nd_seg, lv, rv );
+				v = this->op( lv, rv );
 			}
 			return v;
 		}
 	}
 
 public:
-	seg_tree( int lb, int ub, function<T(int)> init_v, function<T( const segment&, const T&, const T& )> op ){
+	seg_tree( int lb, int ub, function<T(int)> init_v, function<T( const T&, const T& )> op ){
 		int mem_n = 4 * ( ub - lb );
 
 		this->op = op;
@@ -221,66 +246,79 @@ public:
 };
 
 int main(){
-	typedef mod_int_fd< 990244353 > fd_t;
-	const fd_t one( 1 );
+	typedef mod_int_fd< 998244353 > fd_t;
+	typedef tuple< int, tuple<int, int>, tuple<int, int>> collision_t;
+	typedef tuple< int, int, fd_t > proton_t;
+	typedef mtx_t<fd_t, 2> dirc_prob_t;
+
 	int n;
-	vector< tuple<int, int, quotient_t> > proton;
 
-	typedef tuple<int, quotient_t, quotient_t> collision_t;
-
+	vector< proton_t > proton;
 	vector< collision_t > collision;
-
 
 	cin >> n;
 	for( int i = 0; i < n; i ++ ){
 		int x, v, p;
 		cin >> x >> v >> p;
-		proton.push_back( { x, v, fd.div( p, 100 ) } );
+		proton.push_back( { x, v, fd_t( p ) / 100 } );
 	}
 
 	for( int i = 0; i < n - 1; i ++ ){
 		auto [ x, v, p ] = proton[ i ];
 		auto [ nx, nv, np ] = proton[ i + 1 ];
 
-		quotient_t dt = quotient( nx - x, v + nv );
-
-		int r = fd.mul( p, fd.sub( 1, np ) );
-
-		collision.push_back( { i, r, dt} );
+		collision.push_back( { i, {0, 1}, { nx - x, v + nv } } );
 
 		if( v != nv ){
 			if( nv > v ){
-				quotient_t dt  = quotient( nx - x, nv - v );
-				int r = fd.mul( fd.sub( 1, p ), fd.sub( 1, np ));
-
-				collision.push_back( { i, r, dt });
+				collision.push_back( { i, {1, 1}, { nx - x, nv - v } });
 			}else{
-				quotient_t dt  = quotient( nx - x, v - nv );
-				int r = fd.mul( p, np );
-
-				collision.push_back( { i, r, dt });
+				collision.push_back( { i, {0, 0}, { nx - x, v - nv } });
 			}
 		}
 	}
 
 	sort( collision.begin(), collision.end(), []
 		(const collision_t &a, const collision_t &b) -> bool {
-			auto [ a_i, a_p, a_t ] = a;
-			auto [ b_i, b_p, b_t ] = b;
-			return lt( a_t, b_t );
+			auto [ a_i, a_dirc, a_t ] = a;
+			auto [ b_i, b_dirc, b_t ] = b;
+			auto [ atn, atd ] = a_t;
+			auto [ btn, btd ] = b_t;
+			return  ((long long int)atn) * ((long long int)btd)  <
+				 ((long long int)btn) * ((long long int)atd) ;
 		});
 
-	typedef mtx<fd_t, 2, 2> dirc_prob_t;
-	const dirc_prob_t sum_row{ {one, one}, {one, one}};
-	seg_tree<dirc_prob_t> st( 0, n - 1,
-			[ &proton ](int p) -> dirc_prob_t{
-				auto [ x, v, p ] = proton[ p ];
-				auto [ nx, nv, np ] = proton[ p + 1 ];
-				return dirc_prob_t{ { p * np, p * ( one - np )}, { ( one - p) * np, (one - p) * (one - np)} };
+	seg_tree<dirc_prob_t> st( 0, n,
+			[ &proton ](int i) -> dirc_prob_t{
+				auto [ x, v, p ] = proton[ i ];
+				return dirc_prob_t{ { p , p }, { (1 - p), (1 - p) } };
 			},
-			[]( const tuple<int, int> &s, const dirc_prob_t& a, const dirc_prob_t& b ){
-				return 
+			[]( const dirc_prob_t& a, const dirc_prob_t& b ) -> dirc_prob_t {
+				return a * b;
 			});
+
+	fd_t exp_t( 0 );
+	fd_t prob_prev = st.query( 0, n ).sum() / 2;
+
+	for( auto [ i, dirc, qt ] : collision ){
+		auto[ qt_n, qt_d ] = qt;
+		auto[ mi, mj ] = dirc;
+
+		fd_t t = fd_t( qt_n ) / fd_t( qt_d );
+		
+		dirc_prob_t m = st.query( i, i + 1 );
+
+		m.entry[ mi ][ mj ] = fd_t( 0 );
+
+		st.update( i, m );
+		fd_t prob_ex_this = st.query( 0, n ).sum() / 2;
+
+		exp_t = exp_t + ( ( prob_prev - prob_ex_this ) * t );
+
+		prob_prev = prob_ex_this;
+	}
+
+	cout << *(exp_t) << endl;
 
 	return 0;
 }
