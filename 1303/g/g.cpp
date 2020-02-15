b10d06af299d9c374daaf1591af793af52adb7c3
gn0uchat <gn0uchat@gmail.com>
2020-02-15 12:16:09 +0000

    start solving problem 1303 g

---
+++ 1303/g/g.cpp
@@ -0,0 +1,17 @@
+#include<map>
+#include<set>
+#include<cstdio>
+#include<algorithm>
+#include<vector>
+#include<utility>
+#include<string>
+#include<functional>
+#include<queue>
+#include<iostream>
+#include<list>
+#include<tuple>
+using namespace std;
+
+int main(){
+	return 0;
+}
