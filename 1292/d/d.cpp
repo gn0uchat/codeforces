b5b2dcd36823f72471bb7201366a11087a5db359
gn0uchat <gn0uchat@gmail.com>
2020-02-15 07:48:30 +0000

    problem 1292 d AC

--- 1292/d/d.cpp
+++ 1292/d/d.cpp
@@ -24,7 +24,7 @@ private:
 			for( int i = 2; i <= max_prime; i ++ ){
 				if( not_prime[ i ] == false ){
 					_prime.push_back( i );
-					for( int j = i + i; j <= max_prime; not_prime[ j ] = true, j ++ );
+					for( int j = i + i; j <= max_prime; not_prime[ j ] = true, j += i );
 				}
 			}
 		}
@@ -32,7 +32,7 @@ private:
 	}
 
 public:
-	list <factr_t> factr;
+	vector <factr_t> factr;
 
 	fnumb_t(){}
 	fnumb_t( int v ){
@@ -42,9 +42,11 @@ public:
 				break;
 			}
 			for(; v > 1 && (v % f == 0 ); p ++, v = v / f );
-			factr.push_front( { f, p } );
+			factr.push_back( { f, p } );
 		}
+		reverse( factr.begin(), factr.end() );
 	}
+
 
 	friend bool operator== ( const fnumb_t& a, const fnumb_t& b ){
 		if( a.factr.size() != b.factr.size() ){
@@ -52,7 +54,7 @@ public:
 		}else{
 			auto a_it = a.factr.begin();
 			auto b_it = b.factr.begin();
-			for( a_it != a.factr.end() && b_it != b.factr.end(); ++ a_it, ++ b_it ){
+			for(; a_it != a.factr.end() && b_it != b.factr.end(); ++ a_it, ++ b_it ){
 				auto[ a_prm, a_pow ] = *a_it;
 				auto[ b_prm, b_pow ] = *b_it;
 				if( a_prm != b_prm || a_pow != b_pow ){
@@ -63,7 +65,39 @@ public:
 		}
 	}
 
+	friend bool eq_after ( const fnumb_t& a, const fnumb_t& b, int prm_i ){
+		if( a.factr.size() != b.factr.size() ){
+			return false;
+		}else{
+			int n = a.factr.size();
+			for( int i = prm_i; i < n; i ++ ){
+				auto[ a_prm, a_pow ] = a.factr[ i ];
+				auto[ b_prm, b_pow ] = b.factr[ i ];
+				if( a_prm != b_prm || a_pow != b_pow ){
+					return false;
+				}
+			}	
+			return true;
+		}
+	}
+
+	friend bool is_suffix( const fnumb_t& s, const fnumb_t& b, int prm_i ){
+		if( s.factr.size() != b.factr.size() - prm_i ){
+			return false;
+		}else{
+			for( int i = 0; i < s.factr.size(); i ++ ){
+				auto[ s_prm, s_pow ] = s.factr[ i ];
+				auto[ b_prm, b_pow ] = b.factr[ i + prm_i ];
+				if( s_prm != b_prm || s_pow != b_pow ){
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
 	friend fnumb_t operator/ ( const fnumb_t& a, const fnumb_t& b ){
+		fnumb_t c;
 		auto a_it = a.factr.begin();
 		auto b_it = b.factr.begin();
 		while( a_it != a.factr.end() && b_it != b.factr.end() ){
@@ -123,6 +157,9 @@ public:
 		for(; b_it != b.factr.end(); c.factr.push_back( *b_it ), ++ b_it );
 		return c;
 	}
+int prm_size() const {
+		return this->factr.size();
+	}
 
 	int size() const {
 		int l = 0;
@@ -133,25 +170,34 @@ public:
 		return l;
 	}
 
-	friend fnumb_t common_factr( const fnumb_t& a, const fnumb_t& b ){
+	friend fnumb_t common_factr_after( const fnumb_t& a, const fnumb_t& b, int prm_i ){
 		fnumb_t c;
-		auto a_it = a.factr.begin();
-		auto b_it = b.factr.begin();
-		for( a_it != a.factr.end() && b_it != b.factr.end(); ++ a_it, ++ b_it ){
-			auto[ a_prm, a_pow ] *a_it;
-			auto[ b_prm, b_pow ] *b_it;
+		int n = min( a.factr.size(), b.factr.size() );
+		for( int i = prm_i; i < n; i ++ ){
+			auto[ a_prm, a_pow ] = a.factr[ i ];
+			auto[ b_prm, b_pow ] = b.factr[ i ];
 			if( a_prm == b_prm ){
 				if( a_pow != b_pow ){
-					c.push_back( { a_prm, min( a_pow, b_pow ) } );
+					c.factr.push_back( { a_prm, min( a_pow, b_pow ) } );
 					break;
 				}else{
-					c.push_back( { a_prm, a_pow } );
+					c.factr.push_back( { a_prm, a_pow } );
 				}
 			}else{
 				break;
 			}
 		}
 		return c;
+	}
+
+	string str() const{
+		string s;
+		char buf[ 36 ];
+		for( auto[ f, p ] : this->factr ){
+			sprintf( buf, "%d^%d ", f, p );
+			s.append( buf );
+		}
+		return s;
 	}
 
 
@@ -168,11 +214,12 @@ int main(){
 	cin >> n;
 
 	int max_k = 0;
-	vector<int> fragm_k;
+	map<int, int> fragm_ks;
+
 	for( int i = 0; i < n; i ++ ){
 		int k;
 		cin >> k;
-		fragm_k.push_back( k );
+		fragm_ks[ k ] ++;
 		max_k = max( max_k, k );
 	}
 
@@ -183,52 +230,69 @@ int main(){
 
 	typedef tuple<fnumb_t, int, int, list<int>> node_t;
 
-	int nd_mem_i = 0;
-	vector< node_t >nd_mem( 2 * n + 1 );
+	int nd_n = 0;
+	int max_nd_n = 2 * fragm_ks.size() + 1;
+	vector< node_t >nd_mem( max_nd_n + 1 );
 
-	nd_mem[ nd_mem_i ++ ] = { fnumb_t( 1 ), 0, 0, list<int>() };
+	nd_mem[ nd_n ++ ] = { fnumb_t( 1 ), 0, 0, list<int>() };
 
-	funciton<void(const fnumb_t&)> insert_node = [&]( int nd_i, const fnumb_t& v, int agg ){
+	function<void(int, const fnumb_t&, int, int)> insert_node;
+	insert_node = [&]( int nd_i, const fnumb_t& v, int agg, int prm_i ){
 		auto& [ nd_v, nd_c, nd_fth, nd_cld ] = nd_mem[ nd_i ];
-		if( nd_v == v ){
+		if( eq_after( nd_v, v, prm_i ) ){
 			nd_c = nd_c + agg;
 		}else{
+			nd_c = nd_c + agg;
+
 			bool done = false;
 			for( auto &i : nd_cld ){
 				auto& [ sb_v, sb_c, sb_fth, sb_cld ] = nd_mem[ i ];
-				auto cm_v = common_factr( sb_v, v );
-				if( cm_v == sb_v ){
-					done = true;
 
-					insert_node( i, v );
-					break;
-				}else if( !( cm_v == nd_v) ){
-					done = true;
+				auto nd_cm_v = common_factr_after( nd_v, v, prm_i );
+				auto cm_v = common_factr_after( sb_v, v, prm_i );
 
-					int j = nd_mem_i;
-					nd_mem[ nd_mem_i ++ ]= { cm_v, sb_c + 1, nd_i, list<int>{i}};
+				if( nd_cm_v.size() != cm_v.size() ){
+					if( is_suffix( cm_v, sb_v, prm_i ) ){
+						done = true;
 
-					i = j;
-					sb_fth = j;
-					insert_node( j, v );
-					break;
+						insert_node( i, v, agg, prm_i + cm_v.prm_size() - 1 );
+						break;
+					}else{
+						done = true;
+
+						int j = nd_n ++;
+						auto full_cm_v = common_factr_after( sb_v, v, 0 );;
+						nd_mem[ j ]= { full_cm_v, sb_c, nd_i, list<int>{i}};
+
+						i = j;
+						sb_fth = j;
+						insert_node( j, v, agg, prm_i + cm_v.prm_size() - 1 );
+						break;
+					}
 				}
+				
 			}
 
 			if( done == false ){
-				int j = nd_mem_i;
-				nd_mem[ nd_mem_i ++ ] = { v, 1, nd_i, list<int>() };
+				int j = nd_n ++;
+				nd_mem[ j ] = { v, agg, nd_i, list<int>() };
 				nd_cld.push_back( j );
 			}
 		}
 	};
 
-	vector< tuple<bool, int> > _nd_cost( 2 * n + 1 );
-	vector< tuple<bool, int> > _nd_up_cost( 2 * n + 1 );
+	vector< tuple<bool, long long int> > _nd_cost( max_nd_n, {false, 0} );
+	vector< tuple<bool, long long int> > _nd_up_cost( max_nd_n, {false, 0} );
 
-	function<int(int)> nd_cost, nd_up_cost;
+	function<long long int(int)> nd_cost, nd_up_cost;
 
-	nd_cost = [&]( int nd_i ) -> int {
+	auto edge_cost = [&]( int nd_i ) -> long long int {
+		auto[ v, c, fth, cld ] = nd_mem[ nd_i ];
+		auto[ fth_v, fth_c, fth_fth, fth_cld ] = nd_mem[ fth ];
+		return ( ((long long int) v.size() ) - ( (long long int) fth_v.size()) );
+	};
+
+	nd_cost = [&]( int nd_i ) -> long long int {
 		auto& [ done, v ] = _nd_cost[ nd_i ];
 		if( done == true ){
 			return v;
@@ -237,15 +301,15 @@ int main(){
 			auto& [ nd_v, nd_c, nd_fth, nd_cld ] = nd_mem[ nd_i ];
 			v = 0;
 
-			for( auto& i : nd_cld ){
-				auto[ v, c, fth, cld ] = nd_mem[ i ];
-				v = v + nd_cost( i ) + c * ( v.size() - nd_v.size() );
+			for( auto& cld_i : nd_cld ){
+				auto[ cld_v, cld_c, cld_fth, cld_cld ] = nd_mem[ cld_i ];
+				v = v + nd_cost( cld_i ) + ( ( long long int ) cld_c ) * edge_cost( cld_i );
 			}
 			return v;
 		}
 	};
 
-	nd_up_cost = [&]( int nd_i ) -> int {
+	nd_up_cost = [&]( int nd_i ) -> long long int {
 		auto& [ done, v ] = _nd_up_cost[ nd_i ];
 
 		if( done == true ){
@@ -257,28 +321,28 @@ int main(){
 			if( nd_fth == nd_i ){
 				v = 0;
 			}else{
-				v = 0;
+				auto& [ fth_v, fth_c, fth_fth, fth_cld ] = nd_mem[ nd_fth ];
+				long long int sb_cost =
+					nd_cost( nd_fth ) - ( nd_cost( nd_i ) + edge_cost( nd_i ) * ( (long long int) nd_c ));
 
-				auto& [ v, c, fth, cld ] = nd_mem[ nd_fth ];
-
-				for( auto i : cld ){
-					if( i != nd_i ){
-						v = v + nd_cost( i );
-					}
-				}
-
-				v = v + nd_up_cost( nd_fth );
+				v =  edge_cost( nd_i ) * ( (long long int) ( n - nd_c ))
+					+ sb_cost
+					+ nd_up_cost( nd_fth );
 			}
 			return v;
 		}
+	};
+
+	for( auto [ k, c ] : fragm_ks ){
+		insert_node( 0, factrl[ k ], c, 0 );
 	}
 
-	
-
-	for( auto k : fragm_k ){
-		insert_node( 0, factrl( k ) );
+	long long int min_cost = nd_cost( 0 ) + nd_up_cost( 0 );
+	for( int i = 1; i < nd_n; i ++ ){
+		min_cost = min( min_cost, nd_cost( i ) + nd_up_cost( i ) );
 	}
 
+	cout << min_cost << endl;
 
 	return 0;
 }
