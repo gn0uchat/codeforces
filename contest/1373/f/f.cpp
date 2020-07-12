01c070e2b7175069947311559e2c40aa91430efe
gn0uchat <gn0uchat@gmail.com>
2020-07-12 16:05:35 +0800

    TLE

--- contest/1373/f/f.cpp
+++ contest/1373/f/f.cpp
@@ -22,54 +22,88 @@ int main(){
         cin >> n;
 
         for( int i = 0; i < n; i ++ ){
+            int house_holds;
             cin >> house_holds;
             a.push_back( house_holds );
         }
 
         for( int i = 0; i < n; i ++ ){
+            int capacity;
             cin >> capacity;
-            b.push_back( capacity )
+            b.push_back( capacity );
         }
 
-        function<bool(int, int)> under_dist = [&](int i, int dist) -> bool {
-            if( dist < 0 ){
+        function<tuple<bool, int>(int, int)> over_supply = [&]
+        (int i, int previous_supply) -> tuple<bool, int> {
+            bool over = true;
+            int supply = previous_supply;
+
+            for( int i = 0; i < n - 1; i ++ ){
+                if( supply + b[ i ] < a[ i ] ){
+                    over = false;
+                    break;
+                }
+                int satisfied = min( supply, a[ i ] );
+                supply = b[ i ] - ( a[ i ] - satisfied );
+            }
+            return make_tuple( over, supply );
+        /*
+            if( i == n - 1 ){
                 return true;
             }else{
-                if( i + 1 < n ){
-                    return over_dist( i+1, dist + ( b[ i+1 ] - a[ i+1 ] ))
-                }else{
+                if( previous_supply + b[ i ] < a[ i ] ){
                     return false;
+                }else{
+                    int satisfied = min( previous_supply, a[ i ] );
+                    int next_supply = b[ i ] - ( a[ i ] - satisfied );
+                    return over_supply( i + 1, next_supply );
                 }
             }
-        }
+        */
+        };
 
-        int min = -1, max = b[0] + 1;
+        function<bool(int, int, int)> valid_supply =
+        [&](int init_supply, int i, int previous_supply) -> bool {
+            int back_supply = b[ n-1 ] - init_supply;
+            auto[ over, supply ] = over_supply( 0, init_supply );
+
+            return over && back_supply + supply >= a[ n-1 ];
+            
+        /*
+            if( i == n - 1 ){
+                int back_supply = b[ n-1 ] - init_supply;
+                return previous_supply + back_supply >= a[ n-1 ];
+            }else{
+                if( previous_supply + b[ i ] < a[ i ] ){
+                    return false;
+                }else{
+                    int satisfied = min( previous_supply, a[ i ] );
+                    int next_supply = b[ i ] - ( a[ i ] - satisfied );
+                    return valid_supply( init_supply, i + 1, next_supply );
+                }
+            }
+        */
+        };
+
+        int min = -1, max = b[ n-1 ]; 
+
         while( max - min > 1 ){
             int mid = ( min + max ) / 2;
-            if( under_dist( mid ) ){
-                min = mid;
-            }else{
+            auto [over, supply] = over_supply( 0, mid );
+            if( over ){
                 max = mid;
+            }else{
+                min = mid;
             }
         }
 
+        int opt_supply = max;
 
-        auto valid_dist = [&](int i, int dist) -> bool {
-            return 0 <= dist && dist <= b[ i ];
+        if( valid_supply( opt_supply, 0, opt_supply ) ){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" << endl;
         }
-
-        
-        int dist = max;
-        bool valid = valid_dist( 0, dist );
-
-        for( int i = 1; i < n && valid ; i ++ ){
-            dist = dist + ( b[ i ] - a[ i ] );
-            if( ! valid_dist( i, dist ) ){
-                valid = false;
-            }
-        }
-        
-        cout << valid ? "YES" : "NO" << endl;
     }
 
 	return 0;
